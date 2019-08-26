bf5593476e20666ba40e35a158e0c56fc72694ce
UcheSylvester <okorocode@gmail.com>
2019-08-26 23:43:13 +0100

    built a template for product list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { Component } from "@angular/core";
   `
 })
 export class AppComponent {
-  pageTitle: string = "UCylvester Product Management System";
+  pageTitle: string = "UC Product Management System";
 }
