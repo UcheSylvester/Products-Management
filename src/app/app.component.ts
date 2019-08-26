e5374fdb1de20cb4fbbfcbaca06c9857179e231f
UcheSylvester <okorocode@gmail.com>
2019-08-27 00:02:08 +0100

    created product-list component and linked it to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from "@angular/core";
-
 @Component({
   selector: "pm-root",
   template: `
     <div>
       <h1>{{ pageTitle }}</h1>
-      <p>My First Component!</p>
+      <pm-products></pm-products>
     </div>
   `
 })
