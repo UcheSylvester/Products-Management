f63791ebbf12e5054183b396fc8ebaa6984bd39c
UcheSylvester <okorocode@gmail.com>
2019-09-03 01:25:15 +0100

    regitering services with providedIn prop

--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from "@angular/core";
 
 import { IProduct } from "./products";
 
-@Injectable()
+@Injectable({
+  providedIn: "root"
+})
 export class ProductService {
   getProducts(): IProduct[] {
     return [
