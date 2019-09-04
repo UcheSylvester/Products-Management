e4b33aef1a5fbf382a1ba2c04754e3eccf01d3ea
UcheSylvester <okorocode@gmail.com>
2019-09-04 04:08:18 +0100

    working on single product page

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 import { IProduct } from "./products";
 
@@ -9,9 +10,21 @@ import { IProduct } from "./products";
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = "Product Detail";
   product: IProduct;
-  constructor() {}
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
     console.log("product-details onInit");
+    let id = +this.route.snapshot.paramMap.get("id");
+    this.pageTitle += ` ${id}`;
+    this.product = {
+      productId: 10,
+      productName: "Video Game Controller",
+      productCode: "GMG-0042",
+      releaseDate: "October 15, 2018",
+      description: "Standard two-button video game controller",
+      price: 35.95,
+      starRating: 4.6,
+      imageUrl: "assets/images/xbox-controller.png"
+    };
   }
 }
